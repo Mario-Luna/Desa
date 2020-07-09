c13b4d07a9322335a62ca19468a4448a2fe62583
mario_luna <mario_luna@marlue.com.mx>
2020-07-08 22:19:20 -0500

    agregando codigo

---
+++ MiWebb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiWebb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
